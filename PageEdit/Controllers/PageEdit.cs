5ec2d3ff950185029d72aad81a99cc521ea94e5d
YetaWF <mikevdm@YetaWF.com>
2016-09-12 18:59:48 -0400

    rename GoogleAnalytics to Analytics

--- PageEdit/Controllers/PageEdit.cs
+++ PageEdit/Controllers/PageEdit.cs
@@ -141,11 +141,10 @@ namespace YetaWF.Modules.PageEdit.Controllers {
             [UIHint("Text40"), StringLength(PageDefinition.MaxCssClass), CssClassesValidationAttribute, Trim]
             public string CssClass { get; set; }
 
-            [Category("Addons"), Caption("Google Analytics"), Description("The Universal Analytics tracking code used by Google Analytics - You can obtain the tracking code from Google Analytics - Make sure to copy the ENTIRE tracking code (including all javascript and markup) - If omitted, the site defined tracking code is used (Site Settings)")]
-            [TextAbove("Google Analytics is only available in deployed production sites and is ignored in debug builds (not marked deployed).")]
-            [UIHint("TextArea"), AdditionalMetadata("SourceOnly", true), AllowHtml, StringLength(SiteDefinition.MaxGoogleAnalytics), Trim]
-            [HelpLink("https://analytics.google.com/")]
-            public string GoogleAnalytics { get; set; }
+            [Category("Addons"), Caption("Analytics"), Description("Add analytics code (for example, the Universal Analytics tracking code used by Google Analytics or the code used by Clicky) - Any code that should be added at the end of the HTML page can be added here - If omitted, the site defined analytics code is used (Site Settings)")]
+            [TextAbove("Analytics code is only available in deployed production sites and is ignored in debug builds (not marked deployed).")]
+            [UIHint("TextArea"), AdditionalMetadata("SourceOnly", true), AllowHtml, StringLength(SiteDefinition.MaxAnalytics), Trim]
+            public string Analytics { get; set; }
 
             [Category("Meta"), Caption("Meta Tags"), Description("Defines <meta> tags that are added to the page - If specified, this replaces the site meta tags defined using the PageMetaTags property (Site Settings)")]
             [UIHint("TextArea"), AdditionalMetadata("SourceOnly", true), AllowHtml, StringLength(SiteDefinition.MaxMeta), Trim]
