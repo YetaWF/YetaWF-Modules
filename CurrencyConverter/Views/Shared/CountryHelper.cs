f1128a8da1fad9b49c3b8ff2afdad2dd47996724
YetaWF <mikevdm@YetaWF.com>
2016-09-19 12:58:17 -0400

    use browser control in popup so dropdown is not clipped by containing popup

--- CurrencyConverter/Views/Shared/CountryHelper.cs
+++ CurrencyConverter/Views/Shared/CountryHelper.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
 using YetaWF.Core.Pages;
+using YetaWF.Core.Support;
 using YetaWF.Core.Views.Shared;
 using YetaWF.Modules.CurrencyConverter.DataProvider;
 
@@ -13,12 +14,15 @@ namespace YetaWF.Modules.CurrencyConverter.Views.Shared {
 
     public static class CountryHelper {
 
+        private static YetaWFManager Manager { get { return YetaWFManager.Manager; } }
+
         public static MvcHtmlString RenderCountry<TModel>(this HtmlHelper<TModel> htmlHelper, string name, string model, object HtmlAttributes = null) {
 
             using (ExchangeRateDataProvider dp = new ExchangeRateDataProvider()) {
                 ExchangeRateData data = dp.GetItem();
                 List<SelectionItem<string>> list = (from r in data.Rates orderby r.CurrencyName select new SelectionItem<string> { Text = r.CurrencyName, Value = r.Code }).ToList();
-                return htmlHelper.RenderDropDownSelectionList(name, model, list, HtmlAttributes: HtmlAttributes);
+                /* If this is a popup, use regular browser dropdown as the kendo dropdownlist is clipped by the containing window */
+                return htmlHelper.RenderDropDownSelectionList(name, model, list, HtmlAttributes: HtmlAttributes, BrowserControls: Manager.IsInPopup);
             }
         }
     }
