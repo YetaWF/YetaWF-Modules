84bd0460cce0ca286b977cd179512769ae8dcf66
YetaWF <mikevdm@YetaWF.com>
2016-09-22 08:31:31 -0400

    Devtest for scroller template

---
+++ DevTests/Controllers/TemplateScroller.cs
@@ -0,0 +1,52 @@
+/* Copyright © 2016 Softel vdm, Inc. - http://yetawf.com/Documentation/YetaWF/DevTests#License */
+
+using System.Collections.Generic;
+using System.Web.Mvc;
+using YetaWF.Core.Addons;
+using YetaWF.Core.Controllers;
+using YetaWF.Core.Models.Attributes;
+
+namespace YetaWF.Modules.DevTests.Controllers {
+
+    public class TemplateScrollerModuleController : ControllerImpl<YetaWF.Modules.DevTests.Modules.TemplateScrollerModule> {
+
+        public TemplateScrollerModuleController() { }
+
+        public class Item {
+            [UIHint("Image")]
+            public string Image { get; set; }
+            [UIHint("String")]
+            public string Title { get; set; }
+            [UIHint("String")]
+            public string Summary { get; set; }
+        }
+
+        [Trim]
+        public class Model {
+
+            // The Scroller template is a core template implementing the overall Scroller
+            // The AdditionalMetadata describes the user-defined template used for each item in the Scroller
+            [UIHint("Scroller"), ReadOnly, AdditionalMetadata("Template", "YetaWF_DevTests_ScrollerItem")]
+            public List<Item> Items { get; set; }
+
+            public Model() {
+                Items = new List<Item>();
+            }
+        }
+
+        [HttpGet]
+        public ActionResult TemplateScroller() {
+            Model model = new Model { };
+            string addonUrl = VersionManager.GetAddOnModuleUrl(AreaRegistration.CurrentPackage.Domain, AreaRegistration.CurrentPackage.Product);
+            // generate some random data for the scroller items
+            for (int index = 0 ; index < 12 ; ++index) {
+                model.Items.Add(new Item {
+                    Image = string.Format("{0}Images/Image{1}.png", Manager.CurrentSite.MakeUrl(Manager.GetCDNUrl(addonUrl)), index),
+                    Title = string.Format("Item {0}", index),
+                    Summary = string.Format("Summary for item {0} - Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ", index),
+                });
+            }
+            return View(model);
+        }
+    }
+}
---
+++ DevTests/Modules/TemplateScrollerModule.cs
@@ -0,0 +1,45 @@
+﻿/* Copyright © 2016 Softel vdm, Inc. - http://yetawf.com/Documentation/YetaWF/DevTests#License */
+
+using System;
+using YetaWF.Core.IO;
+using YetaWF.Core.Localize;
+using YetaWF.Core.Models.Attributes;
+using YetaWF.Core.Modules;
+using YetaWF.Core.Serializers;
+using YetaWF.DataProvider;
+
+namespace YetaWF.Modules.DevTests.Modules {
+
+    public class TemplateScrollerModuleDataProvider : ModuleDefinitionDataProvider<Guid, TemplateScrollerModule>, IInstallableModel { }
+
+    [ModuleGuid("{4f4d9110-369c-441d-bf55-adc210fa4bc0}")]
+    [UniqueModule(UniqueModuleStyle.NonUnique)]
+    public class TemplateScrollerModule : ModuleDefinition {
+
+        public TemplateScrollerModule() {
+            Title = this.__ResStr("modTitle", "Scroller Test Template");
+            Name = this.__ResStr("modName", "Template Test - Scroller");
+            Description = this.__ResStr("modSummary", "Scroller test template");
+        }
+
+        public override IModuleDefinitionIO GetDataProvider() { return new TemplateScrollerModuleDataProvider(); }
+
+        public override SerializableList<AllowedRole> DefaultAllowedRoles { get { return AdministratorLevel_DefaultAllowedRoles; } }
+
+        public ModuleAction GetAction_Display(string url) {
+            return new ModuleAction(this) {
+                Url = string.IsNullOrWhiteSpace(url) ? ModulePermanentUrl : url,
+                Image = "#Display",
+                LinkText = this.__ResStr("displayLink", "Scroller"),
+                MenuText = this.__ResStr("displayText", "Scroller"),
+                Tooltip = this.__ResStr("displayTooltip", "Display the Scroller test"),
+                Legend = this.__ResStr("displayLegend", "Displays the Scroller test"),
+                Style = ModuleAction.ActionStyleEnum.Normal,
+                Category = ModuleAction.ActionCategoryEnum.Read,
+                Mode = ModuleAction.ActionModeEnum.Any,
+                Location = ModuleAction.ActionLocationEnum.ModuleLinks | ModuleAction.ActionLocationEnum.ModuleMenu,
+                SaveReturnUrl = true,
+            };
+        }
+    }
+}
