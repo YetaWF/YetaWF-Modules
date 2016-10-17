ce5690a92725fe46c9cb533675dd4986d1f37cf4
YetaWF <mikevdm@YetaWF.com>
2016-10-17 19:59:38 -0400

    Http Modules dashboard module

---
+++ Dashboard/Controllers/HttpModulesBrowse.cs
@@ -0,0 +1,75 @@
+/* Copyright © 2016 Softel vdm, Inc. - http://yetawf.com/Documentation/YetaWF/Dashboard#License */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using YetaWF.Core.Controllers;
+using YetaWF.Core.DataProvider;
+using YetaWF.Core.Menus;
+using YetaWF.Core.Models;
+using YetaWF.Core.Models.Attributes;
+using YetaWF.Core.Modules;
+using YetaWF.Core.Views.Shared;
+using YetaWF.Modules.Dashboard.Modules;
+
+namespace YetaWF.Modules.Dashboard.Controllers {
+
+    public class HttpModulesBrowseModuleController : ControllerImpl<YetaWF.Modules.Dashboard.Modules.HttpModulesBrowseModule> {
+
+        public class BrowseItem {
+
+            [Caption("Actions"), Description("The available actions")]
+            [UIHint("ActionIcons"), ReadOnly]
+            public MenuList Commands {
+                get {
+                    MenuList actions = new MenuList() { RenderMode = ModuleAction.RenderModeEnum.IconsOnly };
+                    return actions;
+                }
+            }
+            [Caption("Module"), Description("The module name")]
+            [UIHint("String"), ReadOnly]
+            public string Name { get; set; }
+
+            private HttpModulesBrowseModule Module { get; set; }
+
+            public BrowseItem(HttpModulesBrowseModule module, string name) {
+                Module = module;
+                Name = name;
+            }
+        }
+
+        public class BrowseModel {
+            [UIHint("Grid")]
+            public GridDefinition GridDef { get; set; }
+        }
+
+        [HttpGet]
+        public ActionResult HttpModulesBrowse() {
+            BrowseModel model = new BrowseModel { };
+            model.GridDef = new GridDefinition {
+                AjaxUrl = GetActionUrl("HttpModulesBrowse_GridData"),
+                ModuleGuid = Module.ModuleGuid,
+                RecordType = typeof(BrowseItem),
+                SettingsModuleGuid = Module.PermanentGuid,
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult HttpModulesBrowse_GridData(int skip, int take, List<DataProviderSortInfo> sort, List<DataProviderFilterInfo> filters, Guid settingsModuleGuid) {
+            HttpApplication httpApps = HttpContext.ApplicationInstance;
+            HttpModuleCollection httpModuleCollections = httpApps.Modules;
+            List<BrowseItem> items = (from k in httpModuleCollections.AllKeys select new BrowseItem(Module, k)).ToList();
+            int total = items.Count;
+            items = DataProviderImpl<BrowseItem>.GetRecords(items, skip, take, sort, filters, out total);
+            GridHelper.SaveSettings(skip, take, sort, filters, settingsModuleGuid);
+            return GridPartialView(new DataSourceResult {
+                Data = items.ToList<object>(),
+                Total = total
+            });
+        }
+    }
+}
---
+++ Dashboard/Modules/HttpModulesBrowse.cs
@@ -0,0 +1,44 @@
+﻿/* Copyright © 2016 Softel vdm, Inc. - http://yetawf.com/Documentation/YetaWF/Dashboard#License */
+
+using System;
+using YetaWF.Core.IO;
+using YetaWF.Core.Localize;
+using YetaWF.Core.Models.Attributes;
+using YetaWF.Core.Modules;
+using YetaWF.Core.Serializers;
+using YetaWF.DataProvider;
+
+namespace YetaWF.Modules.Dashboard.Modules {
+
+    public class HttpModulesBrowseModuleDataProvider : ModuleDefinitionDataProvider<Guid, HttpModulesBrowseModule>, IInstallableModel { }
+
+    [ModuleGuid("{e3adcdfa-24ed-423e-9d7c-01541b1bca70}")]
+    [UniqueModule(UniqueModuleStyle.NonUnique)]
+    public class HttpModulesBrowseModule : ModuleDefinition {
+
+        public HttpModulesBrowseModule() {
+            Title = this.__ResStr("modTitle", "Http Modules");
+            Name = this.__ResStr("modName", "Http Modules");
+            Description = this.__ResStr("modSummary", "Displays all Http Modules (Pipeline)");
+        }
+
+        public override IModuleDefinitionIO GetDataProvider() { return new HttpModulesBrowseModuleDataProvider(); }
+
+        public override SerializableList<AllowedRole> DefaultAllowedRoles { get { return AdministratorLevel_DefaultAllowedRoles; } }
+
+        public ModuleAction GetAction_Items(string url) {
+            return new ModuleAction(this) {
+                Url = string.IsNullOrWhiteSpace(url) ? ModulePermanentUrl : url,
+                Image = "#Browse",
+                LinkText = this.__ResStr("browseLink", "Http Modules"),
+                MenuText = this.__ResStr("browseText", "Http Modules"),
+                Tooltip = this.__ResStr("browseTooltip", "Display all Http Modules (Pipeline)"),
+                Legend = this.__ResStr("browseLegend", "Displays all Http Modules (Pipeline)"),
+                Style = ModuleAction.ActionStyleEnum.Normal,
+                Location = ModuleAction.ActionLocationEnum.NoAuto,
+                Category = ModuleAction.ActionCategoryEnum.Read,
+                Mode = ModuleAction.ActionModeEnum.Any,
+            };
+        }
+    }
+}
