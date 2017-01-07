77fdf3fac42c7b042eb6275fac96699d5c8f6656
YetaWF <mikevdm@YetaWF.com>
2017-01-07 12:45:55 -0500

    AreaRegistration change to support parameterless constructor

--- Packages/Startup/AreaRegistration.cs
+++ Packages/Startup/AreaRegistration.cs
@@ -3,8 +3,8 @@
 using YetaWF.Core.Packages;
 
 namespace YetaWF.Modules.Packages.Controllers {
-    public class AreaRegistration : YetaWF.Core.Controllers.AreaRegistration {
-        public AreaRegistration() : base(out CurrentPackage) { }
-        public static new Package CurrentPackage;
+    public class AreaRegistration : YetaWF.Core.Controllers.AreaRegistrationBase {
+        public AreaRegistration() : base() { CurrentPackage = this.GetCurrentPackage(); }
+        public static Package CurrentPackage;
     }
 }
