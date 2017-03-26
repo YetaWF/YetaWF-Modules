673dfe80ccc7cd391396170c28d29c9762162b3a
YetaWF <mikevdm@YetaWF.com>
2017-03-26 16:29:42 -0400

    appsettings.json

--- Blog/Controllers/Disqus.cs
+++ Blog/Controllers/Disqus.cs
@@ -62,7 +62,7 @@ namespace YetaWF.Modules.Blog.Controllers {
                     }
                     string logoffUrl = WebConfigHelper.GetValue<string>("MvcApplication", "LogoffUrl", null);
                     if (string.IsNullOrWhiteSpace(logoffUrl))
-                        throw new InternalError("MvcApplication LogoffUrl not defined in web.cofig - This is required to log off the current user");
+                        throw new InternalError("MvcApplication LogoffUrl not defined in web.cofig/appsettings.json - This is required to log off the current user");
                     model.LogoffUrl = Manager.CurrentSite.MakeUrl(logoffUrl + Manager.CurrentPage.EvaluatedCanonicalUrl);
                 }
                 return View(model);
