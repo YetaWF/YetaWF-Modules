15ef7f6235fe76ac653929af4b9573e5f6b4a834
YetaWF <mikevdm@YetaWF.com>
2017-04-03 11:54:04 -0400

    prepare for 2.0.1

--- Blog/Properties/AssemblyInfo.cs
+++ Blog/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using YetaWF.PackageAttributes;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.0.1.0")]
 
 [assembly: Package(PackageTypeEnum.Module, "YetaWF")]
 [assembly: PackageInfo("http://YetaWF.com/UpdateServer",
