ca675a0aaf07930334916765c776e07b6f66a2c8
YetaWF <mikevdm@YetaWF.com>
2017-05-21 11:37:04 -0400

    move language init back into languages package (low level service provider)

---
+++ Languages/Startup/LanguageSection.cs
@@ -0,0 +1,27 @@
+﻿/* Copyright © 2017 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Languages#License */
+
+using System.IO;
+using YetaWF.Core;
+using YetaWF.Core.Support;
+
+namespace YetaWF.Modules.Languages.Startup {
+
+    public class LanguageSection : IInitializeApplicationStartup {
+
+        // IInitializeApplicationStartup
+        // IInitializeApplicationStartup
+        // IInitializeApplicationStartup
+
+        public void InitializeApplicationStartup() {
+            string rootFolder;
+#if MVC6
+            rootFolder = YetaWFManager.RootFolderWebProject;
+#else
+            rootFolder = YetaWFManager.RootFolder;
+#endif
+            YetaWF.Core.Language.LanguageSection.Init(Path.Combine(rootFolder, Globals.DataFolder, LanguageSettingsFile));
+        }
+
+        public const string LanguageSettingsFile = "LanguageSettings.json";
+    }
+}
