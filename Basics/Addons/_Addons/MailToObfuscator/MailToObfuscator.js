bc7c9b3cf3f943fa7d1b115216136e5aa46e4d39
YetaWF <mikevdm@YetaWF.com>
2017-06-07 17:59:56 -0400

    change for unified page sets

--- Basics/Addons/_Addons/MailToObfuscator/MailToObfuscator.js
+++ Basics/Addons/_Addons/MailToObfuscator/MailToObfuscator.js
@@ -1,18 +1,26 @@
 ﻿/* Copyright © 2017 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Basics#License */
 
 // http://stackoverflow.com/questions/483212/effective-method-to-hide-email-from-spam-bots
-function YGenMailTo(name, domain, subject, linkText) {
-    'use strict';
-    var $a = $('<a>')
-    $a.addClass('ymailto');
-    if (linkText === undefined)
-        linkText = "{0}@{1}".format(name, domain);
-    $a.text(linkText);
-    if (subject !== undefined) {
-        subject = subject.replace(' ', '+');
-        subject = $('<div/>').html(subject + '').text()
-    } else
-        subject = ""
-    $a.attr('href', 'mailto:{0}@{1}?subject={2}'.format(name, domain, subject))
-    document.write($a[0].outerHTML)
-}
+YetaWF_Basics.whenReady.push({
+    callback: function ($tag) {
+        // find all <a> YGenMailTo tags and format an email address
+        $('a.YGenMailTo', $tag).each(function () {
+            var $this = $(this);
+            var addr = $this.attr('data-name') + "@" + $this.attr('data-domain');
+            var s = 'mailto:' + addr;
+            var subj = $this.attr('data-subject');
+            if (subj !== undefined) {
+                subj = subj.replace(' ', '+');
+                subj = $('<div/>').html(subj + '').text();
+                s += '?subject=' + subj;
+            }
+            var text = $this.attr('data-text');
+            if (text === undefined)
+                $this.text(addr);
+            else
+                $this.text(text);
+            $this.attr('href', s);
+        });
+    }
+});
+
