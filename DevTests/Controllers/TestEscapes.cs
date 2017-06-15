8d1d2ba1299998c30aac5e005ee53d95ea91f9a6
YetaWF <mikevdm@YetaWF.com>
2017-06-14 20:13:19 -0400

    more test cases

--- DevTests/Controllers/TestEscapes.cs
+++ DevTests/Controllers/TestEscapes.cs
@@ -16,8 +16,11 @@ namespace YetaWF.Modules.DevTests.Controllers {
         public TestEscapesModuleController() { }
 
         [Trim]
+        [Header("Special characters in the header (HeaderAttribute) < > & @ {0}")]
         public class EditModel {
 
+            [TextAbove("Special characters in the text above the field (TextAboveAttribute) < > & @ {0}")]
+            [TextBelow("Special characters in the text below the field (TextAboveAttribute) < > & @ {0}")]
             [Caption("Caption < > & @ {0}"), Description("A description < > & @ {0}")]
             [UIHint("Text80"), StringLength(80), Trim]
             public string String1 { get; set; }
