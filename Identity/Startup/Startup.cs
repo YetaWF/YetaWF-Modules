ead573efe5b65c34309796241306b2a7a3a8c3e4
YetaWF <mikevdm@YetaWF.com>
2017-08-15 08:34:35 -0400

    asp.net core 2.0 release

--- Identity/Startup/Startup.cs
+++ Identity/Startup/Startup.cs
@@ -2,7 +2,8 @@
 
 #if MVC6
 
-using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.DependencyInjection;
 using System;
@@ -10,6 +11,7 @@ using YetaWF.Core.Identity;
 using YetaWF.Core.Support;
 using YetaWF.Modules.Identity.DataProvider;
 using YetaWF.Modules.Identity.Models;
+
 
 namespace YetaWF.Modules.Identity
 {
@@ -33,17 +35,12 @@ namespace YetaWF.Modules.Identity
                 options.Password.RequireUppercase = WebConfigHelper.GetValue<bool>(AREA, "Password:RequireUppercase");
                 options.Password.RequireLowercase = WebConfigHelper.GetValue<bool>(AREA, "Password:RequireLowercase");
 
-                long secIntvl = WebConfigHelper.GetValue<long>(AREA, "OWin:SecurityStampValidationInterval", new TimeSpan(0, 30, 0).Ticks); // 30 minutes
-                //$$ options.SecurityStampValidationInterval = new TimeSpan(secIntvl);
+                // long secIntvl = WebConfigHelper.GetValue<long>(AREA, "OWin:SecurityStampValidationInterval", new TimeSpan(0, 30, 0).Ticks); // 30 minutes
                 
                 // We handle lockouts
                 options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(0);
                 options.Lockout.MaxFailedAccessAttempts = 0;
 
-                // Cookie settings
-                //$$ options.Cookies.ApplicationCookieAuthenticationScheme = IdentityCookieOptions.ApplicationScheme;
-                //$$ options.Cookies.ExternalCookieAuthenticationScheme = IdentityCookieOptions.ExternalScheme;
-                
                 // User settings
                 // the default is acceptable: options.User.AllowedUserNameCharacters
                 // We handle email/name ourselves
@@ -51,27 +48,34 @@ namespace YetaWF.Modules.Identity
             });
 
             services.ConfigureApplicationCookie(c => {
-                c.CookieName = string.Format(".YetaWF.Cookies.{0}", YetaWFManager.DefaultSiteName);
                 long ticks = WebConfigHelper.GetValue<long>(AREA, "OWin:ExpireTimeSpan", new TimeSpan(10, 0, 0, 0).Ticks);
+                c.Cookie.Domain = YetaWFManager.DefaultSiteName;
+                c.Cookie.Expiration = new TimeSpan(ticks);
+                c.Cookie.Name = string.Format(".YetaWF.Cookies.{0}", YetaWFManager.DefaultSiteName);
+                c.Cookie.SecurePolicy = Microsoft.AspNetCore.Http.CookieSecurePolicy.None;
                 c.ExpireTimeSpan = new TimeSpan(ticks);
-                c.CookieSecure = Microsoft.AspNetCore.Http.CookieSecurePolicy.None;
                 c.SlidingExpiration = WebConfigHelper.GetValue<bool>(AREA, "OWin:SlidingExpiration", true);
             });
             services.ConfigureExternalCookie(c => {
-                c.CookieName = string.Format(".YetaWF.Cookies.Ext.{0}", YetaWFManager.DefaultSiteName);
                 long ticks = WebConfigHelper.GetValue<long>(AREA, "OWin:ExpireTimeSpan", new TimeSpan(10, 0, 0, 0).Ticks);
+                c.Cookie.Domain = YetaWFManager.DefaultSiteName;
+                c.Cookie.Expiration = new TimeSpan(ticks);
+                c.Cookie.Name = string.Format(".YetaWF.Cookies.Ext.{0}", YetaWFManager.DefaultSiteName);
+                c.Cookie.SecurePolicy = Microsoft.AspNetCore.Http.CookieSecurePolicy.None;
                 c.ExpireTimeSpan = new TimeSpan(ticks);
-                c.CookieSecure = Microsoft.AspNetCore.Http.CookieSecurePolicy.None;
                 c.SlidingExpiration = WebConfigHelper.GetValue<bool>(AREA, "OWin:SlidingExpiration", true);
             });
         }
 
         public void SetupLoginProviders(IServiceCollection services) {
+            // https://github.com/aspnet/Security/issues/1310
+
+            AuthenticationBuilder authBuilder = services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme);
             {
                 string pub = WebConfigHelper.GetValue<string>(AREA, "FacebookAccount:Public");
                 string priv = WebConfigHelper.GetValue<string>(AREA, "FacebookAccount:Private");
                 if (!string.IsNullOrWhiteSpace(pub) && !string.IsNullOrWhiteSpace(priv)) {
-                    services.AddFacebookAuthentication("Facebook", o => {
+                    authBuilder.AddFacebook(o => {
                         o.AppId = pub;
                         o.AppSecret = priv;
                     });
@@ -81,7 +85,7 @@ namespace YetaWF.Modules.Identity
                 string pub = WebConfigHelper.GetValue<string>(AREA, "GoogleAccount:Public");
                 string priv = WebConfigHelper.GetValue<string>(AREA, "GoogleAccount:Private");
                 if (!string.IsNullOrWhiteSpace(pub) && !string.IsNullOrWhiteSpace(priv)) {
-                    services.AddGoogleAuthentication("Google", o => {
+                    authBuilder.AddGoogle(o => {
                         o.ClientId = pub;
                         o.ClientSecret = priv;
                     });
@@ -91,7 +95,7 @@ namespace YetaWF.Modules.Identity
                 string pub = WebConfigHelper.GetValue<string>(AREA, "TwitterAccount:Public");
                 string priv = WebConfigHelper.GetValue<string>(AREA, "TwitterAccount:Private");
                 if (!string.IsNullOrWhiteSpace(pub) && !string.IsNullOrWhiteSpace(priv)) {
-                    services.AddTwitterAuthentication("Twitter", o => {
+                    authBuilder.AddTwitter(o => {
                         o.ConsumerKey = pub;
                         o.ConsumerSecret = priv;
                     });
@@ -101,7 +105,7 @@ namespace YetaWF.Modules.Identity
                 string pub = WebConfigHelper.GetValue<string>(AREA, "MicrosoftAccount:Public");
                 string priv = WebConfigHelper.GetValue<string>(AREA, "MicrosoftAccount:Private");
                 if (!string.IsNullOrWhiteSpace(pub) && !string.IsNullOrWhiteSpace(priv)) {
-                    services.AddMicrosoftAccountAuthentication("Microsoft", o => {
+                    authBuilder.AddMicrosoftAccount(o => {
                         o.ClientId = pub;
                         o.ClientSecret = priv;
                     });
