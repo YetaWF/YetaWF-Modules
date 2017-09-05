aa24f518429425090c667cbd77caf350bcdc21f5
YetaWF <mikevdm@YetaWF.com>
2017-09-04 22:03:16 -0400

    last deployed date incorrect (check wrong folder on asp.net core)

--- Dashboard/Controllers/Status.cs
+++ Dashboard/Controllers/Status.cs
@@ -52,7 +52,7 @@ namespace YetaWF.Modules.Dashboard.Controllers {
             if (corePackage != null)
                 model.CoreVersion = corePackage.Version;
             if (Manager.Deployed)
-                model.LastDeploy = Directory.GetCreationTimeUtc(Path.Combine(YetaWFManager.RootFolder, Globals.NodeModulesFolder));
+                model.LastDeploy = Directory.GetCreationTimeUtc(Path.Combine(YetaWFManager.RootFolderWebProject, Globals.NodeModulesFolder));
 #if DEBUG
             model.Build = "Debug";
 #else
