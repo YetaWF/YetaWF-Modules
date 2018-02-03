d93850e586846e380f800552a7544d077c3f7061
YetaWF <mikevdm@YetaWF.com>
2018-02-02 21:54:14 -0500

    add dashboard dataprovider module

---
+++ Dashboard/Controllers/BrowseDataProviders.cs
@@ -0,0 +1,71 @@
+/* Copyright © 2018 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Dashboard#License */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using YetaWF.Core.Controllers;
+using YetaWF.Core.DataProvider;
+using YetaWF.Core.Models;
+using YetaWF.Core.Models.Attributes;
+using YetaWF.Core.Views.Shared;
+using YetaWF.Modules.Dashboard.DataProvider;
+#if MVC6
+using Microsoft.AspNetCore.Mvc;
+#else
+using System.Web.Mvc;
+#endif
+
+namespace YetaWF.Modules.Dashboard.Controllers {
+
+    public class BrowseDataProvidersModuleController : ControllerImpl<YetaWF.Modules.Dashboard.Modules.BrowseDataProvidersModule> {
+
+        public class BrowseItem {
+
+            [Caption("I/O Mode"), Description("The I/O mode supported by this data provider")]
+            [UIHint("String"), ReadOnly]
+            public string IOModeName { get; set; }
+            [Caption("Type Name"), Description("The type of the supported data provider")]
+            [UIHint("String"), ReadOnly]
+            public string TypeName { get; set; }
+            [Caption("Implementation Type Name"), Description("The type of the implementation of the supported data provider")]
+            [UIHint("String"), ReadOnly]
+            public string TypeImplName { get; set; }
+
+            public BrowseItem(DataProviderInfo data) {
+                ObjectSupport.CopyData(data, this);
+            }
+        }
+
+        public class BrowseModel {
+            [Caption(""), Description("")] // empty entries required so property is shown in property list (but with a suppressed label)
+            [UIHint("Grid"), ReadOnly]
+            public GridDefinition GridDef { get; set; }
+        }
+
+        [AllowGet]
+        public ActionResult BrowseDataProviders() {
+            BrowseModel model = new BrowseModel { };
+            model.GridDef = new GridDefinition {
+                AjaxUrl = GetActionUrl("BrowseDataProviders_GridData"),
+                ModuleGuid = Module.ModuleGuid,
+                RecordType = typeof(BrowseItem),
+                SettingsModuleGuid = Module.PermanentGuid,
+            };
+            return View(model);
+        }
+
+        [AllowPost]
+        [ConditionalAntiForgeryToken]
+        public ActionResult BrowseDataProviders_GridData(int skip, int take, List<DataProviderSortInfo> sort, List<DataProviderFilterInfo> filters, Guid settingsModuleGuid) {
+            using (DataProviderInfoDataProvider dataProvider = new DataProviderInfoDataProvider()) {
+                int total;
+                List<DataProviderInfo> browseItems = dataProvider.GetItems(skip, take, sort, filters, out total);
+                GridHelper.SaveSettings(skip, take, sort, filters, settingsModuleGuid);
+                return GridPartialView(new DataSourceResult {
+                    Data = (from s in browseItems select new BrowseItem(s)).ToList<object>(),
+                    Total = total
+                });
+            }
+        }
+    }
+}
---
+++ Dashboard/Modules/BrowseDataProviders.cs
@@ -0,0 +1,46 @@
+/* Copyright © 2018 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Dashboard#License */
+
+using System;
+using YetaWF.Core.IO;
+using YetaWF.Core.Localize;
+using YetaWF.Core.Models.Attributes;
+using YetaWF.Core.Modules;
+using YetaWF.DataProvider;
+#if MVC6
+using Microsoft.AspNetCore.Mvc;
+#else
+#endif
+
+namespace YetaWF.Modules.Dashboard.Modules {
+
+    public class BrowseDataProvidersModuleDataProvider : ModuleDefinitionDataProvider<Guid, BrowseDataProvidersModule>, IInstallableModel { }
+
+    [ModuleGuid("{bb4f1bf1-eebf-4e65-8992-c4f673737c26}")]
+    [UniqueModule(UniqueModuleStyle.NonUnique)]
+    public class BrowseDataProvidersModule : ModuleDefinition {
+
+        public BrowseDataProvidersModule() {
+            Title = this.__ResStr("modTitle", "Data Providers");
+            Name = this.__ResStr("modName", "Data Providers");
+            Description = this.__ResStr("modSummary", "Displays installed data providers");
+            DefaultViewName = StandardViews.PropertyListEdit;
+        }
+
+        public override IModuleDefinitionIO GetDataProvider() { return new BrowseDataProvidersModuleDataProvider(); }
+
+        public ModuleAction GetAction_Items(string url) {
+            return new ModuleAction(this) {
+                Url = string.IsNullOrWhiteSpace(url) ? ModulePermanentUrl : url,
+                Image = "#Browse",
+                LinkText = this.__ResStr("browseLink", "Data Providers"),
+                MenuText = this.__ResStr("browseText", "Data Providers"),
+                Tooltip = this.__ResStr("browseTooltip", "Display installed data providers"),
+                Legend = this.__ResStr("browseLegend", "Displays  installed data providers"),
+                Style = ModuleAction.ActionStyleEnum.Normal,
+                Location = ModuleAction.ActionLocationEnum.NoAuto,
+                Category = ModuleAction.ActionCategoryEnum.Read,
+                Mode = ModuleAction.ActionModeEnum.Any,
+            };
+        }
+    }
+}
