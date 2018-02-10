e9c05255421d9db2e5dd06b05de611a15ff7588f
YetaWF <mikevdm@YetaWF.com>
2018-02-10 17:31:08 -0500

    I suck at simple changes

--- PageEar/Addons/_Main/jquery.peelback.js
+++ PageEar/Addons/_Main/jquery.peelback.js
@@ -58,7 +58,7 @@
 
             //Assemble
             //&&&&&&&&&&&&&&&&&&& CUSTOMIZATION:  id="peelback" & rel=
-            peelHTML = $('<div id="peelback"><a href="' + base.settings.clickURL + '" class="peelback" target="_blank" rel='noopener noreferrer'><img src="' + base.settings.peelImage + '" alt="" border="0" /></a><div></div></div>');
+            peelHTML = $('<div id="peelback"><a href="' + base.settings.clickURL + '" class="peelback" target="_blank" rel="noopener noreferrer"><img src="' + base.settings.peelImage + '" alt="" border="0" /></a><div></div></div>');
             peelImage = peelHTML.find('img');
             peelMask = peelHTML.find('div');
 
