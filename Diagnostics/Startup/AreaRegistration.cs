5e88d062a41c2e87d5fe64b5a1d6603641f30297
YetaWF <mikevdm@YetaWF.com>
2018-04-08 14:29:32 -0400

    old

--- Diagnostics/Startup/AreaRegistration.cs
+++
@@ -1,8 +0,0 @@
-using YetaWF.Core.Packages;
-
-namespace YetaWF.Modules.Diagnostics.Controllers {
-    public class AreaRegistration : YetaWF.Core.Controllers.AreaRegistrationBase { 
-        public AreaRegistration() : base() { CurrentPackage = this.GetCurrentPackage(); }
-        public static Package CurrentPackage;
-    }
-}
