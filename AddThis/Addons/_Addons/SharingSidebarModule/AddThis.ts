501ed34185247f144a2064c62838bbf4e4af9b2d
YetaWF <mikevdm@YetaWF.com>
2018-10-16 19:48:53 -0400

    typescript

---
+++ AddThis/Addons/_Addons/SharingSidebarModule/AddThis.ts
@@ -0,0 +1,12 @@
+/* Copyright © 2018 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/AddThis#License */
+
+// Handles events turning the addon on/off (used for dynamic content)
+$YetaWF.registerContentChange((addonGuid: string, on: boolean) : void => {
+    if (addonGuid === "d790d324-ec41-419d-abba-fdb03652cd9d") {
+        var addThis = $YetaWF.getElement1BySelector("div.addthis-smartlayers");
+        if (on)
+            addThis.style.display = "block";
+        else
+            addThis.style.display = "none";
+    }
+});
