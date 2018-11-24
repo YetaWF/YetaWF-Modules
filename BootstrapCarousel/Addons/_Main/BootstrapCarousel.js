4f81a6a872c8c3f1f829b0482cd55ee02cf2a70a
YetaWF <mikevdm@YetaWF.com>
2018-11-23 20:55:21 -0500

    cleanup empty files

--- BootstrapCarousel/Addons/_Main/BootstrapCarousel.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-/* Copyright � 2018 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/BootstrapCarousel#License */
-
-//# sourceMappingURL=BootstrapCarousel.js.map
--- BootstrapCarousel/Addons/_Templates/SlideShow/SlideShowEdit.ts
+++ BootstrapCarousel/Addons/_Templates/SlideShow/SlideShowEdit.ts
@@ -1,5 +1,14 @@
 /* Copyright © 2018 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/BootstrapCarousel#License */
 
+namespace YetaWF {
+    export interface IConfigs {
+        YetaWF_BootstrapCarousel: YetaWF_BootstrapCarousel.IPackageConfigs;
+    }
+    export interface ILocs {
+        YetaWF_BootstrapCarousel: YetaWF_BootstrapCarousel.IPackageLocs;
+    }
+}
+
 namespace YetaWF_BootstrapCarousel {
 
     export interface IPackageConfigs {
