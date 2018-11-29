8ec94323ddc3af33e9205a4309063c3d9f4dd482
YetaWF <mikevdm@YetaWF.com>
2018-11-29 18:37:07 -0500

    module menu handling, js -> ts, no jquery

--- ComponentsHTML/Addons/_Addons/Modules/Modules.ts
+++ ComponentsHTML/Addons/_Addons/Modules/Modules.ts
@@ -1,105 +1,117 @@
-"use strict";
 /* Copyright © 2018 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/ComponentsHTML#License */
-var YetaWF_ComponentsHTML;
-(function (YetaWF_ComponentsHTML) {
-    var MenuHandler = /** @class */ (function () {
-        function MenuHandler() {
-        }
-        MenuHandler.clearMenus = function (force) {
+
+namespace YetaWF_ComponentsHTML {
+
+    export class MenuHandler {
+
+        public static ClearInterval: number = 0;
+
+        public static clearMenus(force: boolean): void {
             if (!force) {
                 // if we still have a current module, we can't clear menus
                 if ($YetaWF.getElement1BySelectorCond(".yModule.yModule-current"))
                     return;
             }
+
             // clear the interval
             if (MenuHandler.ClearInterval)
                 clearInterval(MenuHandler.ClearInterval);
             MenuHandler.ClearInterval = 0;
+
             var editIcons = $YetaWF.getElementsBySelector(".yModuleMenuEditIcon");
             if (editIcons.length === 0)
                 return; // modules don't necessarily have an edit menu
+
             var menus = $YetaWF.getElementsBySelector(".yModuleMenu");
-            for (var _i = 0, menus_1 = menus; _i < menus_1.length; _i++) {
-                var menu = menus_1[_i];
+            for (let menu of menus) {
                 // hide all module menus
                 menu.style.display = "none";
                 // there is no longer a current module
                 $YetaWF.elementRemoveClass(menu, "yModule-current");
             }
-            for (var _a = 0, editIcons_1 = editIcons; _a < editIcons_1.length; _a++) {
-                var editIcon = editIcons_1[_a];
+
+            for (let editIcon of editIcons) {
                 if (force) {
                     editIcon.style.display = "none";
-                }
-                else {
+                } else {
                     ComponentsHTMLHelper.fadeOut(editIcon, 200);
                 }
             }
-        };
-        MenuHandler.registerMouseEnterHandlers = function () {
+        }
+
+        public static registerMouseEnterHandlers(): void {
             var mods = $YetaWF.getElementsBySelector(".yModule");
-            for (var _i = 0, mods_1 = mods; _i < mods_1.length; _i++) {
-                var mod = mods_1[_i];
-                $YetaWF.registerEventHandler(mod, "mouseenter", null, function (ev) {
+            for (let mod of mods) {
+                $YetaWF.registerEventHandler(mod, "mouseenter", null, (ev: MouseEvent): boolean => {
                     return MenuHandler.onHandleModuleMouseEnter(ev);
                 });
-                $YetaWF.registerEventHandler(mod, "mouseleave", null, function (ev) {
+                $YetaWF.registerEventHandler(mod, "mouseleave", null, (ev: MouseEvent): boolean => {
                     return MenuHandler.onHandleModuleMouseLeave(ev);
                 });
             }
             var editIcons = $YetaWF.getElementsBySelector(".yModuleMenuEditIcon");
-            for (var _a = 0, editIcons_2 = editIcons; _a < editIcons_2.length; _a++) {
-                var editIcon = editIcons_2[_a];
-                $YetaWF.registerEventHandler(editIcon, "mouseenter", null, function (ev) {
+            for (let editIcon of editIcons) {
+                $YetaWF.registerEventHandler(editIcon, "mouseenter", null, (ev: MouseEvent): boolean => {
                     return MenuHandler.onHandleEditIconMouseEnter(ev);
                 });
             }
-        };
-        MenuHandler.onHandleModuleMouseEnter = function (ev) {
+        }
+        private static onHandleModuleMouseEnter(ev: MouseEvent): boolean {
             //console.log("Entering module");
-            var modDiv = ev.__YetaWFElem;
+
+            var modDiv = ev.__YetaWFElem as HTMLDivElement;
             // check if this module is already current
             if ($YetaWF.elementHasClass(modDiv, "yModule-current"))
                 return true;
+
             // find the module's edit icon
             var editIcon = $YetaWF.getElement1BySelectorCond(".yModuleMenuEditIcon", [modDiv]);
-            if (!editIcon) // it just doesn't have a menu
+            if (!editIcon)// it just doesn't have a menu
                 return true;
+
             // find the module's menu
             var menu = $YetaWF.getElement1BySelector(".yModuleMenu", [modDiv]);
             // if the edit icon is already visible, we're done
             if ($YetaWF.isVisible(menu))
                 return true;
+
             // entered a new module - clear all module menus that may be open
             MenuHandler.clearMenus(true);
+
             // add a class to the module to identify it's the current module
             $YetaWF.elementAddClass(modDiv, "yModule-current");
+
             //if (YVolatile.Basics.EditModeActive) { }
+
             // fade in edit icon
             ComponentsHTMLHelper.fadeIn(editIcon, 500);
-            MenuHandler.ClearInterval = setInterval(function () { MenuHandler.clearMenus(false); }, 1500);
+
+            MenuHandler.ClearInterval = setInterval((): void => { MenuHandler.clearMenus(false); }, 1500);
+
             return true;
-        };
-        MenuHandler.onHandleModuleMouseLeave = function (ev) {
+        }
+        private static onHandleModuleMouseLeave(ev: MouseEvent): boolean {
+
             //console.log("Exiting module");
-            var modDiv = ev.__YetaWFElem;
+
+            var modDiv = ev.__YetaWFElem as HTMLDivElement;
             $YetaWF.elementRemoveClass(modDiv, "yModule-current");
             return true;
-        };
+        }
+
         /** Show/hide menu as we're hovering over the edit icon */
-        MenuHandler.onHandleEditIconMouseEnter = function (ev) {
+        private static onHandleEditIconMouseEnter(ev: MouseEvent): boolean {
+
             //console.log("Entering edit icon");
-            var modDiv = ev.__YetaWFElem;
+
+            var modDiv = ev.__YetaWFElem as HTMLDivElement;
             // find the module's menu
             var menu = $YetaWF.getElement1BySelector(".yModuleMenu", [modDiv]);
+
             menu.style.display = "";
             return true;
-        };
-        MenuHandler.ClearInterval = 0;
-        return MenuHandler;
-    }());
-    YetaWF_ComponentsHTML.MenuHandler = MenuHandler;
-    MenuHandler.registerMouseEnterHandlers();
-})(YetaWF_ComponentsHTML || (YetaWF_ComponentsHTML = {}));
+        }
+    }
 
-//# sourceMappingURL=Modules.js.map
+    MenuHandler.registerMouseEnterHandlers();
+}
