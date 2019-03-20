a5f3d8be91d6f2f0d14a7b548a007c045757d39c
YetaWF <mikevdm@YetaWF.com>
2019-03-20 19:11:08 -0400

    postgresql data providers

---
+++ Dashboard/Models/AuditInfoPostgreSQLDataProvider.cs
@@ -0,0 +1,18 @@
+/* Copyright © 2019 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Dashboard#License */
+
+using System.Collections.Generic;
+using YetaWF.Core.DataProvider;
+using YetaWF.DataProvider.PostgreSQL;
+
+namespace YetaWF.Modules.Dashboard.DataProvider.PostgreSQL {
+
+    public class PostgreSQLDataProvider : IExternalDataProvider {
+
+        public void Register() {
+            DataProviderImpl.RegisterExternalDataProvider(PostgreSQLBase.ExternalName, typeof(DataProvider.AuditInfoDataProvider), typeof(AuditInfoDataProvider));
+        }
+        class AuditInfoDataProvider : PostgreSQLSimpleIdentityObject<int, AuditInfo> {
+            public AuditInfoDataProvider(Dictionary<string, object> options) : base(options) { }
+        }
+    }
+}
---
+++ Feedback/Models/PostgreSQLDataProvider.cs
@@ -0,0 +1,22 @@
+﻿/* Copyright © 2019 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Feedback#License */
+
+using System.Collections.Generic;
+using YetaWF.Core.DataProvider;
+using YetaWF.DataProvider.PostgreSQL;
+
+namespace YetaWF.Modules.Feedback.DataProvider.PostgreSQL {
+
+    public class PostgreSQLDataProvider : IExternalDataProvider {
+
+        public void Register() {
+            DataProviderImpl.RegisterExternalDataProvider(PostgreSQLBase.ExternalName, typeof(DataProvider.FeedbackConfigDataProvider), typeof(FeedbackConfigDataProvider));
+            DataProviderImpl.RegisterExternalDataProvider(PostgreSQLBase.ExternalName, typeof(DataProvider.FeedbackDataProvider), typeof(FeedbackDataProvider));
+        }
+        class FeedbackConfigDataProvider : PostgreSQLSimpleObject<int, FeedbackConfigData> {
+            public FeedbackConfigDataProvider(Dictionary<string, object> options) : base(options) { }
+        }
+        class FeedbackDataProvider : PostgreSQLSimpleObject<int, FeedbackData> {
+            public FeedbackDataProvider(Dictionary<string, object> options) : base(options) { }
+        }
+    }
+}
