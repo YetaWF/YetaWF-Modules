7c11bfc7dc36340326a254e11975fb557e7fd905
YetaWF <mikevdm@YetaWF.com>
2019-04-13 11:50:10 -0400

    js

--- ComponentsHTML/Addons/_Templates/Grid/Grid.js
+++ ComponentsHTML/Addons/_Templates/Grid/Grid.js
@@ -550,7 +550,7 @@ var YetaWF_ComponentsHTML;
             var _this = this;
             if (!this.reloadInProgress) {
                 this.setReloading(true);
-                if (page <= 1)
+                if (page < 1)
                     page = 1;
                 if (this.Setup.StaticData && !sort) {
                     // show/hide selected rows
