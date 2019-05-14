f2830ef9ab44049d7b29392e37c0c886287d8eff
YetaWF <mikevdm@YetaWF.com>
2019-05-14 08:20:18 -0400

    redirect logged on users module

---
+++ Identity/Controllers/RedirectLoggedOnUsers.cs
@@ -0,0 +1,27 @@
+using YetaWF.Core.Controllers;
+using System.Threading.Tasks;
+using YetaWF.Core.Identity;
+#if MVC6
+using Microsoft.AspNetCore.Mvc;
+#else
+using System.Web.Mvc;
+#endif
+
+namespace YetaWF.Modules.Identity.Controllers {
+
+    public class RedirectLoggedOnUsersModuleController : ControllerImpl<YetaWF.Modules.Identity.Modules.RedirectLoggedOnUsersModule> {
+
+        [AllowGet]
+        public async Task<ActionResult> RedirectLoggedOnUsers() {
+
+            if (Manager.HaveUser && !Manager.EditMode) {
+                string nextUrl = await Resource.ResourceAccess.GetUserPostLoginUrlAsync(Manager.UserRoles);
+                if (string.IsNullOrWhiteSpace(nextUrl))
+                    nextUrl = Manager.CurrentSite.PostLoginUrl;
+                if (!string.IsNullOrWhiteSpace(nextUrl))
+                    return RedirectToUrl(nextUrl);
+            }
+            return new EmptyResult();
+        }
+    }
+}
