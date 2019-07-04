57087e45d132870a6c1818760797d23c54a738b7
YetaWF <mikevdm@YetaWF.com>
2019-07-04 13:18:46 -0400

    copyright

--- Identity/Controllers/RedirectLoggedOnUsers.cs
+++ Identity/Controllers/RedirectLoggedOnUsers.cs
@@ -1,3 +1,5 @@
+/* Copyright © 2019 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Identity#License */
+
 using YetaWF.Core.Controllers;
 using System.Threading.Tasks;
 using YetaWF.Core.Identity;
