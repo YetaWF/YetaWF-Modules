a24440f258a4f311d7b7c92bc7eb4b9e28641573
YetaWF <mikevdm@YetaWF.com>
2019-08-21 11:21:34 -0400

    sitepropertiesservice

---
+++ SitePropertiesService/Models/FileDataProvider.cs
@@ -0,0 +1,22 @@
+﻿/* Copyright © 2019 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/SitePropertiesService#License */
+
+using System.Collections.Generic;
+using System.IO;
+using YetaWF.Core.DataProvider;
+using YetaWF.Core.Site;
+using YetaWF.Core.Support;
+using YetaWF.DataProvider;
+
+namespace YetaWF.Modules.SitePropertiesService.DataProvider.File {
+
+    public class FileDataProvider : IExternalDataProvider {
+
+        public void Register() {
+            DataProviderImpl.RegisterExternalDataProvider(FileDataProviderBase.ExternalName, typeof(YetaWF.Modules.SitePropertiesService.Models.SiteDefinitionDataProvider), typeof(SiteDefinitionDataProvider));
+        }
+        class SiteDefinitionDataProvider : FileDataProvider<string, SiteDefinition> {
+            public SiteDefinitionDataProvider(Dictionary<string, object> options) : base(options) { }
+            public override string GetBaseFolder() { return Path.Combine(YetaWFManager.DataFolder, Dataset); }
+        }
+    }
+}
---
+++ SitePropertiesService/Startup/AreaRegistration.cs
@@ -0,0 +1,10 @@
+﻿/* Copyright © 2019 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/SitePropertiesService#License */
+
+using YetaWF.Core.Packages;
+
+namespace YetaWF.Modules.SitePropertiesService.Controllers {
+    public class AreaRegistration : YetaWF.Core.Controllers.AreaRegistrationBase {
+        public AreaRegistration() : base() { CurrentPackage = this.GetCurrentPackage(); }
+        public static Package CurrentPackage;
+    }
+}
