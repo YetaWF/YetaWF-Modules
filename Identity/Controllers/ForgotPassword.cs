128f2ea1d9f876e0ef4f698d2b8288a9a1c6068c
YetaWF <mikevdm@YetaWF.com>
2019-09-22 21:55:16 -0400

    forgot password (name or email)

--- Identity/Controllers/ForgotPassword.cs
+++ Identity/Controllers/ForgotPassword.cs
@@ -12,6 +12,7 @@ using YetaWF.Modules.Identity.Support;
 using YetaWF.Core.Components;
 using System;
 using YetaWF.Core.Identity;
+using YetaWF.Modules.Identity.Modules;
 #if MVC6
 using Microsoft.AspNetCore.Mvc;
 #else
@@ -29,14 +30,28 @@ namespace YetaWF.Modules.Identity.Controllers {
 
             [TextAbove("You can request help recovering your password with your email address. Information will be sent to your email address and should arrive in your inbox within a few minutes. Please make sure to update your spam filters to avoid rejecting this email.")]
             [Caption("Email Address"), Description("Enter the email address associated with your account")]
-            [UIHint("Email"), StringLength(Globals.MaxEmail), Required]
+            [UIHint("Email"), StringLength(Globals.MaxEmail), EmailValidation, Trim]
+            [ProcessIf(nameof(RegistrationType), RegistrationTypeEnum.EmailOnly)]
+            [RequiredIf(nameof(RegistrationType), RegistrationTypeEnum.EmailOnly)]
             public string Email { get; set; }
+
+            [TextAbove("You can request help recovering your password with your account name. Information will be sent to the registered email address and should arrive in your inbox within a few minutes. Please make sure to update your spam filters to avoid rejecting this email.")]
+            [Caption("Name"), Description("Enter the name associated with your account")]
+            [UIHint("Text40"), StringLength(Globals.MaxUser), UserNameValidation, Trim]
+            [ProcessIf(nameof(RegistrationType), RegistrationTypeEnum.NameAndEmail)]
+            [ProcessIf(nameof(RegistrationType), RegistrationTypeEnum.NameOnly)]
+            [RequiredIf(nameof(RegistrationType), RegistrationTypeEnum.NameAndEmail)]
+            [RequiredIf(nameof(RegistrationType), RegistrationTypeEnum.NameOnly)]
+            public string UserName { get; set; }
 
             [Caption("Captcha"), Description("Please verify that you're a human and not a spam bot")]
             [UIHint("RecaptchaV2"), RecaptchaV2("Please verify that you're a human and not a spam bot"), SuppressIf("ShowCaptcha", false)]
             public RecaptchaV2Data Captcha { get; set; }
             [UIHint("Hidden")]
             public bool ShowCaptcha { get; set; }
+
+            [UIHint("Hidden")]
+            public RegistrationTypeEnum RegistrationType { get; set; }
 
             public EditModel() {
                 Captcha = new RecaptchaV2Data();
@@ -48,6 +63,7 @@ namespace YetaWF.Modules.Identity.Controllers {
             LoginConfigData config = await LoginConfigDataProvider.GetConfigAsync();
             EditModel model = new EditModel {
                 ShowCaptcha = config.CaptchaForgotPassword,
+                RegistrationType = config.RegistrationType,
             };
             return View(model);
         }
@@ -62,17 +78,36 @@ namespace YetaWF.Modules.Identity.Controllers {
                 throw new InternalError("Hidden field tampering detected");
 
             model.ShowCaptcha = config.CaptchaForgotPassword;
+            model.RegistrationType = config.RegistrationType;
             if (!ModelState.IsValid)
                 return PartialView(model);
+
             using (UserDefinitionDataProvider userDP = new UserDefinitionDataProvider()) {
-                UserDefinition userDef = await userDP.GetItemByEmailAsync(model.Email);
-                if (userDef == null) {
-                    ModelState.AddModelError("Email", this.__ResStr("badEmail", "According to our records there is no account associated with this email address"));
-                    return PartialView(model);
+
+                UserDefinition userDef;
+                switch (model.RegistrationType) {
+                    case RegistrationTypeEnum.NameOnly:
+                    case RegistrationTypeEnum.NameAndEmail:
+                        userDef = await userDP.GetItemAsync(model.UserName);
+                        if (userDef == null) {
+                            ModelState.AddModelError(nameof(model.UserName), this.__ResStr("badName", "According to our records there is no account associated with this name"));
+                            return PartialView(model);
+                        }
+                        break;
+                    default:
+                    case RegistrationTypeEnum.EmailOnly:
+                        userDef = await userDP.GetItemByEmailAsync(model.Email);
+                        if (userDef == null) {
+                            ModelState.AddModelError(nameof(model.Email), this.__ResStr("badEmail", "According to our records there is no account associated with this email address"));
+                            return PartialView(model);
+                        }
+                        break;
                 }
+
                 using (UserLoginInfoDataProvider logInfoDP = new UserLoginInfoDataProvider()) {
                     if (await logInfoDP.IsExternalUserAsync(Manager.UserId)) {
-                        ModelState.AddModelError("Email", this.__ResStr("extUser", "This account can only be accessed using an external login provider"));
+                        ModelState.AddModelError(nameof(model.Email), this.__ResStr("extUser", "This account can only be accessed using an external login provider"));
+                        ModelState.AddModelError(nameof(model.UserName), this.__ResStr("extUser", "This account can only be accessed using an external login provider"));
                         return PartialView(model);
                     }
                 }
