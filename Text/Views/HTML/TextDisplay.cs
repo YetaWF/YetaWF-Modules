133a599db1f11c7069a50bc166c6ef848151e886
Mike <mikevdm@mikevdm.com>
2020-03-09 09:34:42 -0400

    cleanup Text module and use module specific addons

--- Text/Views/HTML/TextDisplay.cs
+++ Text/Views/HTML/TextDisplay.cs
@@ -1,11 +1,8 @@
 ﻿/* Copyright © 2020 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/Text#License */
 
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using YetaWF.Core.Components;
-using YetaWF.Core.Localize;
 using YetaWF.Core.Packages;
-using YetaWF.Core.Support;
 using YetaWF.Modules.ComponentsHTML.Components;
 using YetaWF.Modules.Text.Controllers;
 using YetaWF.Modules.Text.Modules;
@@ -20,20 +17,7 @@ namespace YetaWF.Modules.Text.Views {
         public override string GetViewName() { return ViewName; }
 
         public async Task<string> RenderViewAsync(TextModule module, TextModuleController.TextModelDisplay model) {
-
-            HtmlBuilder hb = new HtmlBuilder();
-
-            List<FormButton> buttons = new List<FormButton> {
-                new FormButton() { ButtonType= ButtonTypeEnum.Apply, Text=this.__ResStr("btnApply", "Save") },
-            };
-            if (Manager.IsInPopup)
-                buttons.Add(new FormButton() { ButtonType = ButtonTypeEnum.Cancel, });
-            else
-                buttons.Add(new FormButton() { ButtonType = ButtonTypeEnum.Submit, Text = this.__ResStr("btnSave", "Save & Display"), Title = this.__ResStr("btnSaveTT", "Save all text and display the page in Site View mode") });
-
-            hb.Append($@"
-{await HtmlHelper.ForDisplayAsync(model, nameof(model.Contents))}");
-            return hb.ToString();
+            return await HtmlHelper.ForDisplayAsync(model, nameof(model.Contents));
         }
     }
 }
