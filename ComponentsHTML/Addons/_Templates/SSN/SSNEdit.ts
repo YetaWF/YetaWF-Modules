e6cf8678257d70620bd6b9e7ea490cfff6130016
Mike <mikevdm@mikevdm.com>
2020-05-10 08:37:45 -0400

    Social Security component

---
+++ ComponentsHTML/Addons/_Templates/SSN/SSNEdit.ts
@@ -0,0 +1,72 @@
+﻿/* Copyright © 2020 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/ComponentsHTML#License */
+
+namespace YetaWF_ComponentsHTML {
+
+    export class SSNEditComponent extends YetaWF.ComponentBaseDataImpl {
+
+        public static readonly TEMPLATE: string = "yt_ssn";
+        public static readonly SELECTOR: string = ".yt_ssn.t_edit";
+        public static readonly EVENTCHANGE: string = "ssn_change";
+
+        Hidden: HTMLInputElement;
+        SSN: HTMLInputElement;
+        kendoMaskedTextBox: kendo.ui.MaskedTextBox;
+
+        constructor(controlId: string) {
+            super(controlId, SSNEditComponent.TEMPLATE, SSNEditComponent.SELECTOR, {
+                ControlType: ControlTypeEnum.Template,
+                ChangeEvent: SSNEditComponent.EVENTCHANGE,
+                GetValue: (control: SSNEditComponent): string | null => {
+                    return control.valueRaw;
+                },
+                Enable: (control: SSNEditComponent, enable: boolean, clearOnDisable: boolean): void => {
+                    YetaWF_BasicsImpl.elementEnableToggle(control.Hidden, enable);
+                    control.enable(enable);
+                    if (!enable && clearOnDisable)
+                        control.clear();
+                }
+            }, false, (tag: HTMLElement, control: SSNEditComponent): void => {
+                control.kendoMaskedTextBox.destroy();
+            });
+
+            this.Hidden = $YetaWF.getElement1BySelector("input[type=\"hidden\"]", [this.Control]) as HTMLInputElement;
+            this.SSN = $YetaWF.getElement1BySelector("input[name=\"ssninput\"]", [this.Control]) as HTMLInputElement;
+
+            $(this.SSN).kendoMaskedTextBox({
+                mask: "000-00-0000",
+                change: (ev: kendo.ui.MaskedTextBoxChangeEvent): void => {
+                    let kdMask: kendo.ui.MaskedTextBox = ev.sender;
+                    let val = kdMask.raw();
+                    this.setHidden(val);
+                    FormsSupport.validateElement(this.Hidden);
+                    var event = document.createEvent("Event");
+                    event.initEvent(SSNEditComponent.EVENTCHANGE, true, true);
+                    this.Control.dispatchEvent(event);
+                }
+            });
+            this.kendoMaskedTextBox = $(this.SSN).data("kendoMaskedTextBox");
+            this.setHidden(this.kendoMaskedTextBox.raw());
+        }
+        private setHidden(val: string | null): void {
+            this.Hidden.setAttribute("value", val??"");
+        }
+        get valueRaw(): string {
+            return this.kendoMaskedTextBox.raw();
+        }
+        get valueText(): string {
+            return this.kendoMaskedTextBox.value();
+        }
+        set value(val: string) {
+            this.setHidden(val);
+            this.kendoMaskedTextBox.value(val);
+        }
+        public clear(): void {
+            this.setHidden(null);
+            this.kendoMaskedTextBox.value("");
+        }
+        public enable(enabled: boolean): void {
+            this.kendoMaskedTextBox.enable(enabled);
+        }
+    }
+}
+
---
+++ ComponentsHTML/Components/SSN.cs
@@ -0,0 +1,115 @@
+﻿/* Copyright © 2020 Softel vdm, Inc. - https://yetawf.com/Documentation/YetaWF/ComponentsHTML#License */
+
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using YetaWF.Core.Components;
+using YetaWF.Core.Localize;
+using YetaWF.Core.Models.Attributes;
+using YetaWF.Core.Packages;
+using YetaWF.Core.Support;
+
+namespace YetaWF.Modules.ComponentsHTML.Components {
+
+    /// <summary>
+    /// Base class for the SSN component implementation.
+    /// </summary>
+    public abstract class SSNComponentBase : YetaWFComponent {
+
+        internal const string TemplateName = "SSN";
+
+        /// <summary>
+        /// Returns the package implementing the component.
+        /// </summary>
+        /// <returns>Returns the package implementing the component.</returns>
+        public override Package GetPackage() { return Controllers.AreaRegistration.CurrentPackage; }
+        /// <summary>
+        /// Returns the component name.
+        /// </summary>
+        /// <returns>Returns the component name.</returns>
+        /// <remarks>Components in packages whose product name starts with "Component" use the exact name returned by GetTemplateName when used in UIHint attributes. These are considered core components.
+        /// Components in other packages use the package's area name as a prefix. E.g., the UserId component in the YetaWF.Identity package is named "YetaWF_Identity_UserId" when used in UIHint attributes.
+        ///
+        /// The GetTemplateName method returns the component name without area name prefix in all cases.</remarks>
+        public override string GetTemplateName() { return TemplateName; }
+    }
+
+    /// <summary>
+    /// Displays the model formatted as a Social Security Number.
+    /// </summary>
+    public class SSNDisplayComponent : SSNComponentBase, IYetaWFComponent<string> {
+
+        /// <summary>
+        /// Returns the component type (edit/display).
+        /// </summary>
+        /// <returns>Returns the component type.</returns>
+        public override ComponentType GetComponentType() { return ComponentType.Display; }
+
+        /// <summary>
+        /// Called by the framework when the component needs to be rendered as HTML.
+        /// </summary>
+        /// <param name="model">The model being rendered by the component.</param>
+        /// <returns>The component rendered as HTML.</returns>
+        public Task<string> RenderAsync(string model) {
+            HtmlBuilder hb = new HtmlBuilder();
+            if (model != null && model.Length == 9) {
+                hb.Append($@"
+<div class='yt_ssn t_display'>
+    {HE(SSNValidationAttribute.GetDisplay(model))}
+</div>");
+            }
+            return Task.FromResult(hb.ToString());
+        }
+    }
+
+    /// <summary>
+    /// Allows entry of a Social Security Number.
+    /// </summary>
+    public class SSNEditComponent : SSNComponentBase, IYetaWFComponent<string> {
+
+        /// <summary>
+        /// Returns the component type (edit/display).
+        /// </summary>
+        /// <returns>Returns the component type.</returns>
+        public override ComponentType GetComponentType() { return ComponentType.Edit; }
+
+        /// <summary>
+        /// Called by the framework when the component is used so the component can add component specific addons.
+        /// </summary>
+        public override async Task IncludeAsync() {
+            await KendoUICore.AddFileAsync("kendo.maskedtextbox.min.js");
+            await base.IncludeAsync();
+        }
+        /// <summary>
+        /// Called by the framework when the component needs to be rendered as HTML.
+        /// </summary>
+        /// <param name="model">The model being rendered by the component.</param>
+        /// <returns>The component rendered as HTML.</returns>
+        public async Task<string> RenderAsync(string model) {
+
+            UseSuppliedIdAsControlId();
+
+            HtmlBuilder hb = new HtmlBuilder();
+
+            hb.Append($"<div id='{ControlId}' class='yt_ssn t_edit'>");
+
+            Dictionary<string, object> hiddenAttributes = new Dictionary<string, object>(HtmlAttributes) {
+                { "__NoTemplate", true }
+            };
+            hb.Append(await HtmlHelper.ForEditComponentAsync(Container, PropertyName, null, "Hidden", HtmlAttributes: hiddenAttributes, Validation: Validation));
+
+            YTagBuilder tag = new YTagBuilder("input");
+            FieldSetup(tag, FieldType.Anonymous);
+            tag.Attributes.Add("name", "ssninput");
+
+            if (model != null)
+                tag.MergeAttribute("value", model);
+            hb.Append(tag.ToString(YTagRenderMode.StartTag));
+
+            hb.Append($"</div>");
+
+            Manager.ScriptManager.AddLast($@"new YetaWF_ComponentsHTML.SSNEditComponent('{ControlId}');");
+
+            return hb.ToString();
+        }
+    }
+}
