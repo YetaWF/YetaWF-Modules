b05729c71a5ff2ecd06bae2db7f5f38b67c42361
Mike <mikevdm@mikevdm.com>
2020-11-11 19:13:47 -0500

    prepare 5.3.0

--- Pages/Properties/AssemblyInfo.cs
+++ Pages/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using YetaWF.PackageAttributes;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 
-[assembly: AssemblyVersion("5.2.0.0")]
+[assembly: AssemblyVersion("5.3.0.0")]
 
 [assembly: Package(PackageTypeEnum.Module, "YetaWF")]
 [assembly: PackageInfo("https://YetaWF.com/UpdateServer",
