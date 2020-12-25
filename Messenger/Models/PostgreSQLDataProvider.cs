dafd9cc56cb7024ab149cdfe6813c521608f8357
Mike <mikevdm@mikevdm.com>
2020-12-25 09:26:04 -0500

    Messenger: remove old, dead code

--- Messenger/Models/PostgreSQLDataProvider.cs
+++ Messenger/Models/PostgreSQLDataProvider.cs
@@ -9,20 +9,8 @@ namespace YetaWF.Modules.Messenger.DataProvider.PostgreSQL {
     public class PostgreSQLDataProvider : IExternalDataProvider {
 
         public void Register() {
-            //DataProviderImpl.RegisterExternalDataProvider(SQLBase.ExternalName, typeof(DataProvider.ConfigDataProvider), typeof(ConfigDataProvider));
-            //DataProviderImpl.RegisterExternalDataProvider(SQLBase.ExternalName, typeof(DataProvider.ConnectionDataProvider), typeof(ConnectionDataProvider));
-            //DataProviderImpl.RegisterExternalDataProvider(SQLBase.ExternalName, typeof(DataProvider.MessagingDataProvider), typeof(MessagingDataProvider));
             DataProviderImpl.RegisterExternalDataProvider(SQLBase.ExternalName, typeof(DataProvider.SiteAnnouncementDataProvider), typeof(SiteAnnouncementDataProvider));
         }
-        //class ConfigDataProvider : SQLSimpleObject<int, ConfigData> {
-        //    public ConfigDataProvider(Dictionary<string, object> options) : base(options) { }
-        //}
-        //class ConnectionDataProvider : SQLSimpleObject<string, Connection> {
-        //    public ConnectionDataProvider(Dictionary<string, object> options) : base(options) { }
-        //}
-        //class MessagingDataProvider : SQLSimpleIdentityObject<int, Message> {
-        //    public MessagingDataProvider(Dictionary<string, object> options) : base(options) { }
-        //}
         class SiteAnnouncementDataProvider : SQLSimpleIdentityObject<int, SiteAnnouncement> {
             public SiteAnnouncementDataProvider(Dictionary<string, object> options) : base(options) { }
         }
